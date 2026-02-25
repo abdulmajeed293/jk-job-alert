30242cee340e914d7cf5082740ff52a22cb80c2a
Abdul Majeed <majidqureshi7659@gmail.com>
2026-02-25 16:09:32 +0530

    New Update

--- app/jobs/[slug]/page.tsx
+++ app/jobs/[slug]/page.tsx
@@ -239,7 +239,7 @@ export default function JobDetailPage() {
               [&_td]:p-3
               [&_td]:border-b
 
-              [&_tr:nth-child(even)]:bg-blue-500
+              [&_tr:nth-child(even)]:bg-blue-500 text-white
               [&_tr:hover]:bg-blue-100
 
               [&_thead]:bg-blue-100"
